8956e228c2db02cebb6ff53fb8fdcff240e97ed3
vschindl <vschindl@vschindldev.comsoft.de>
2019-10-09 12:24:31 +0200

    update myconfig

--- myconfig.el
+++ myconfig.el
@@ -21,6 +21,16 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; general
 (message "general setup")
 
+(require 'tramp)
+
+ ;; scroll one line at a time (less "jumpy" than defaults)
+    (setq mouse-wheel-scroll-amount '(1 ((shift) . 1))) ;; one line at a time
+    (setq mouse-wheel-progressive-speed nil) ;; don't accelerate scrolling
+    (setq mouse-wheel-follow-mouse 't) ;; scroll window under mouse
+    (setq scroll-step 1) ;; keyboard scroll one line at a time
+    (setq tramp-default-method "ssh")
+
+
 ;; file tree
 (require 'neotree)
 
